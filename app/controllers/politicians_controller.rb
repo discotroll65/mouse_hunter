b915ae9cee1bf3a466e42bcf3394df5048e68e57
Justine Breuch <justinebreuch@Justines-MacBook-Pro.local>
2014-07-03 00:11:29 +0200

    began adding things to the politicians table

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -1,9 +1,11 @@
 class PoliticiansController < ApplicationController 	
 	def index
-		@politicians = Politician.get_politicians 
+		@politicians = Politician.get_politicians
+		Politician.destroy_all 
 		@politicians.each do |politician|
 			Politician.create(name: politician["last_name"])
 		end
+		@politicians = Politician.all
 	end
 
 	def show
