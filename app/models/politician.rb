602815fe347f9cbd6c7f4ba26cf1f3443a7439ff
Garrett Simpson <elgaricimo@gmail.com>
2014-07-04 12:48:09 +0200

    Search zipcode functionality working. Still need to make it so that the search results are not saved in the database if the politician is already there. Deleted the random route files.

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,8 +1,8 @@
 class Politician < ActiveRecord::Base
-	def self.get_politicians
+	def self.get_politicians(zip)
 		
 		#zipcode is hardwired in currently
-		response = RestClient.get("http://congress.api.sunlightfoundation.com/legislators/locate?zip=12009&apikey=64177a5c45dc44eb8752332b15fb89bf")
+		response = RestClient.get("http://congress.api.sunlightfoundation.com/legislators/locate?zip=#{zip}&apikey=64177a5c45dc44eb8752332b15fb89bf")
 		parsed_response = JSON.parse(response)
 		politicians = parsed_response["results"]
 		politicians
