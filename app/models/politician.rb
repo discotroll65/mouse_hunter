ad42814318c00ff3bdef81ad84294a246261f50b
Justine Breuch <justinebreuch@Justines-MacBook-Pro.local>
2014-07-04 16:14:50 +0200

    began work on political figure user page, changed the models so that the profiles change depending on information in the database

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -7,4 +7,47 @@ class Politician < ActiveRecord::Base
 		politicians = parsed_response["results"]
 		politicians
 	end
+
+	def ordinalize
+		number = self.to_i
+        if (11..13).include?(number % 100)
+                "#{self}th"
+        else
+            case number % 10
+                when 1 
+                	"#{self}st"
+                when 2 
+                	"#{self}nd"
+                when 3 
+                	"#{self}rd"
+                else   
+                	"#{self}th"
+            end
+        end
+
+   end
+
+   def symbol
+   		if self.party_finder == "Democrat"
+   			return "donkey.png"
+   		elsif self.party_finder == "Republican"
+   			return "elephant.png"
+   		else 
+   			" "
+   		end
+   end
+
+   def party_finder
+   		party_letter = self.party
+		if party_letter[0] == "D"
+			"Democrat"
+		elsif party_letter[0] == "R"
+			"Republican"
+		elsif party_letter[0] == "I"
+			"Independent"
+		else
+			" "
+		end
+   end
+
 end
