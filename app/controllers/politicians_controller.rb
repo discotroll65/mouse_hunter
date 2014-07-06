db422ef9ec668d55e6cc075ea0575161fb9ee077
Garrett Simpson <elgaricimo@gmail.com>
2014-07-06 17:22:01 +0200

    Made it so that Politicians get updated with basic NYT api info. Put commented out jazz_hands, into gem file.

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -9,9 +9,11 @@ class PoliticiansController < ApplicationController
 		
 		#This goes over an array of hashes containing politicians of the passed-in zipcode and their attributes, and then creates active records of them. A validate uniqueness in the model makes sure that duplicates don't happen
 		Politician.get_politicians(@zipcode).each do |politician|
-			@politicians_zip << Politician.create(name: (politician["first_name"] + " " + politician["last_name"]), first_name: politician["first_name"], last_name: politician["last_name"], district: politician["district"], state: politician["state"], title: politician["title"], twitter_id: politician["twitter_id"], in_office: politician["in_office"], contact_form: politician["contact_form"], party: politician["party"], congress_cid: politician["crp_id"])
+			@politicians_zip << Politician.create(name: (politician["first_name"] + " " + politician["last_name"]), first_name: politician["first_name"], last_name: politician["last_name"], district: politician["district"], state: politician["state"], title: politician["title"], twitter_id: politician["twitter_id"], in_office: politician["in_office"], contact_form: politician["contact_form"], party: politician["party"], congress_cid: politician["crp_id"], chamber: politician["chamber"])
+			Politician.last.get_info_from_NYT
 		end
 		# @politicians = Politician.all
+
 	end
 
 	def show
