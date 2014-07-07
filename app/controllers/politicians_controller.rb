a3283f4470ae54d9948cbf9cc43e5e35c0d74beb
Justine Breuch <justinebreuch@Justines-MacBook-Pro.local>
2014-07-07 11:41:59 +0200

    updated the index page

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -12,7 +12,8 @@ class PoliticiansController < ApplicationController
 
 		Politician.get_politicians(@zipcode).each do |politician|
 
-			@politicians_zip << Politician.create(name: (politician["first_name"] + " " + politician["last_name"]), first_name: politician["first_name"], last_name: politician["last_name"], district: politician["district"], state: politician["state"], title: politician["title"], twitter_id: politician["twitter_id"], in_office: politician["in_office"], contact_form: politician["contact_form"], party: politician["party"], congress_cid: politician["crp_id"], chamber: politician["chamber"], bioguide_id: politician["bioguide_id"])
+			@politicians_zip << Politician.create(name: (politician["first_name"] + " " + politician["last_name"]), first_name: politician["first_name"], last_name: politician["last_name"], district: politician["district"], state: politician["state"], title: politician["title"], twitter_id: politician["twitter_id"], in_office: politician["in_office"], contact_form: politician["contact_form"], party: politician["party"], congress_cid: politician["crp_id"], chamber: politician["chamber"], bioguide_id: politician["bioguide_id"], missed_votes_pct: politician["missed_votes_pct"], facebook_account: politician["facebook_account"], votes_with_party_pct: politician["votes_with_party_pct"])
+
 			Politician.last.get_info_from_NYT
 
 		end
